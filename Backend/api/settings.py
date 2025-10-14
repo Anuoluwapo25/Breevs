d8f7603de17933c45b3fbd0d028a2469340cfe41
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2025-10-14 19:05:45 +0100

    feat: add allowed host

--- Backend/api/settings.py
+++ Backend/api/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-*4wjv%#vh1k23+jh0rej90x4ipukm^o*6@$68djbbsj0)74&xu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
